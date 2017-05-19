46d077ad59b4dbfae34458c74c03aa758780bbe9
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-19 11:13:34 -0700

    Added blog prompts for writing a blog.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('Hello');
+import inquirer from 'inquirer';
+
+const blogPrompts = [
+    {
+        name: 'post',
+        message: 'Write the blog post...',
+        type: 'editor',
+    },
+    {
+        name: 'name',
+        message: 'What is the name of this post?',
+        type: 'input',
+    },
+    {
+        name: 'publish',
+        message: 'Would you like to publish this post?',
+        type: 'confirm',
+        default: false,
+    },
+];
+
+inquirer
+    .prompt(blogPrompts)
+    .then(console.log)
