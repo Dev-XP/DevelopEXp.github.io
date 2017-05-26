e214d853e35cc5b43fdbfe5d98e7d49d877b1a83
Raymond Benefield <Raymond.Benefield@gmail.com>
2017-05-26 08:35:53 -0700

    Convert a command into the designated prompts.

--- src/index.js
+++ src/index.js
@@ -11,31 +11,36 @@ const discoverSubCommand = stream => stream
         }),
     }));
 
+const prompts = {
+    blog: [
+        {
+            name: 'post',
+            message: 'Write the blog post...',
+            type: 'editor',
+        },
+        {
+            name: 'name',
+            message: 'What is the name of this post?',
+            type: 'input',
+        },
+        {
+            name: 'publish',
+            message: 'Would you like to publish this post?',
+            type: 'confirm',
+            default: false,
+        },
+    ],
+};
+
+const getPrompts = stream => stream
+    .map(service => prompts[service.command]);
+
 Observable
     .of(process.argv.slice(2))
     .map(minimist)
     .let(discoverSubCommand)
-    .subscribe(console.log);
-
-const blogPrompts = [
-    {
-        name: 'post',
-        message: 'Write the blog post...',
-        type: 'editor',
-    },
-    {
-        name: 'name',
-        message: 'What is the name of this post?',
-        type: 'input',
-    },
-    {
-        name: 'publish',
-        message: 'Would you like to publish this post?',
-        type: 'confirm',
-        default: false,
-    },
-];
-
-inquirer
-    .prompt(blogPrompts)
-    .then(console.log)
+    .let(getPrompts)
+    .subscribe(prompts => inquirer
+        .prompt(prompts)
+        .then(console.log)
+    );
